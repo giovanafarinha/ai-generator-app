8f11b6b386c528e3625368155cfe030b39e660d7
giovanafarinha8-del <giovanafarinha8@gmail.com>
2025-09-15 16:19:51 +0200

    Added javascript handling and added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generating(event) {
+  event.preventDefault();
+  new Typewriter("#ai-answer", {
+    strings: "Generated answer",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+let generatingFormElement = document.querySelector("#generatorForm");
+generatingFormElement.addEventListener("submit", generating);
